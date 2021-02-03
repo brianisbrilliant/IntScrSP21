8d60eaab0c9c1ac4fa97b3e153d6bfee07e630b2
Abra <aolvera316620@gmail.com>
2021-02-03 15:20:15 -0600

    Added PickUps!

---
+++ Assets/PlayerComponent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerComponent : MonoBehaviour
+{
+    public int Coins = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("I have hit " + other.gameObject.name);
+        if(other.gameObject.CompareTag("Coin"))
+        {
+            //destroy the coin
+            Destroy(other.gameObject);
+            Coins += 1;
+        }
+    }
+}
