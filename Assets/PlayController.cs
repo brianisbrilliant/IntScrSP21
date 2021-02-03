267f98d1cd69d344719d17ab61705159cab8724c
Bosunwill <54271663+Bosunwill@users.noreply.github.com>
2021-02-03 15:29:48 -0600

    added pickup

---
+++ Assets/PlayController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayController : MonoBehaviour
+{
+   public int Coin = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other){
+        Debug.Log("I have hit " + other.gameObject.name);
+        if(other.gameObject.CompareTag("Coin")){
+            Coin = +1;
+            Destroy(other.gameObject);
+        }
+    }
+}
