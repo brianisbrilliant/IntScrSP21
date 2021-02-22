4bbda917f89e9d458f139147859ef60d9a9d9f83
Abra <aolvera316620@gmail.com>
2021-02-22 14:33:44 -0600

    Shooting

---
+++ Assets/Gun.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    //this function will be called from the player controller
+    public void Pickup(Transform hand) {
+        this.gameObject.transform.SetParent(hand);
+        this.transform.localPosition = Vector3.zero;
+        this.transform.localRotation = Quaternion.identity;
+        this.GetComponent<Rigidbody>().isKinematic = true;
+        this.GetComponent<Collider>().enabled = false;
+    }
+
+    public void Use(GameObject Bullet) {
+        Debug.Log("<color=red>Pow!</color>");
+        Transform firePoint;
+        firePoint = GameObject.Find("FirePoint").transform;
+        GameObject bullet;
+        bullet = Instantiate(Bullet, firePoint.position, firePoint.rotation, null);
+        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 200);
+
+    }
+
+    public void Drop() {
+        Debug.Log("Dropping our item.");
+        this.gameObject.transform.SetParent(null);
+        this.transform.Translate(0,0,2);
+        this.GetComponent<Rigidbody>().isKinematic = false;
+        this.GetComponent<Rigidbody>().AddForce(transform.forward * 10, ForceMode.Impulse);
+        this.GetComponent<Collider>().enabled = true;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int coins; 
+    Gun heldItem;
+    // Start is called before the first frame update
+    [SerializeField] GameObject bullet;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")) {
+            Debug.Log("Iv'e pressed Left Mouse Button");
+            if(heldItem != null){
+                heldItem.Use(bullet);
+            } else {
+                Debug.Log("We arent holding anything");
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q)){
+            if (heldItem != null) {
+                heldItem.Drop();
+            } else {
+                Debug.Log("We arent holding anything");
+            }
+        }
+    }
+    [SerializeField]
+    Transform hand; 
+
+   void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("I have hit " + other.gameObject.name + "!");
+
+        if (other.gameObject.CompareTag("Item"))
+        {
+            heldItem = other.GetComponent<Gun>();
+            heldItem.Pickup(hand);
+        }
+
+        if(other.gameObject.CompareTag("Coin"))
+        {
+            Destroy(other.gameObject);
+            coins += 1;
+        }
+
+        if (other.gameObject.CompareTag("Floor")) {
+            other.gameObject.GetComponent<Renderer>().material.color = Random.ColorHSV();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("floor"))
+        {
+            other.gameObject.GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
+}
